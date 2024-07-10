622f13cd267ea925bcce07f149bdf537b8ae9913
Matt <>
2024-07-10 17:38:54 +0200

    First commit : Initialisation du projet

---
+++ src/main/java/fr/enitodo/EnitodoProjetThomasMatthewApplication.java
@@ -0,0 +1,13 @@
+package fr.enitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnitodoProjetThomasMatthewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnitodoProjetThomasMatthewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/enitodo/EnitodoProjetThomasMatthewApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.enitodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnitodoProjetThomasMatthewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
