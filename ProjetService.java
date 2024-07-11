c7cf3a296462be841d023349a1d00ebe6f8b6987
thomas4231555445 <thomasgomez49@gmail.com>
2024-07-11 10:21:00 +0200

    Add files via upload

---
+++ ProjetService.java
@@ -0,0 +1,5 @@
+package fr.enitodo.bll;
+
+public interface ProjetService {
+
+}
---
+++ Taches.java
@@ -0,0 +1,12 @@
+package fr.enitodo.bo;
+
+import java.io.Serializable;
+
+public class Taches implements Serializable  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
